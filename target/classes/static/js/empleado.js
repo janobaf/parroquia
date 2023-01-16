13e1f7b7267eef164dfb0e5673babedf767c310c
unknown <arturo331972@hotmail.com>
2023-01-16 18:46:58 -0500

    make it better4

--- target/classes/static/js/empleado.js
+++
@@ -1,9 +0,0 @@
- /*BUSCAR EMPLEADO POR DNI*/
- function buscar(){
-     var filtro=document.getElementById("filtro").value;
-     const url="listarEmpleado";
-     var urlPartida =window.location.href.split('/');
-     window.location.href=urlPartida[0]+'//'+urlPartida[2]+'/'+url+'/'+filtro;           
-}
-
-
--- target/classes/static/js/libroDetalle.js
+++
@@ -1,8 +0,0 @@
-/*BUSCAR CLIENTE POR DNI*/            
-function buscarDNI(){
-	var filtro=document.getElementById("filtro").value;
-	const url="listarLibroDetalle";
-	var urlPartida =window.location.href.split('/');
-	window.location.href=urlPartida[0]+'//'+urlPartida[2]+'/'+url+'/'+filtro;
-            	
-}
